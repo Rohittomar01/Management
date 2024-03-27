22b0fe09e2a4e9f4e3a5f6293e394a06ef817320
Deepak Singh Tomar <112108549+deepaktomar3352@users.noreply.github.com>
2024-03-27 21:21:56 +0530

    updated ourteam

--- src/Component/OurTeam.js
+++ src/Component/OurTeam.js
@@ -51,12 +51,12 @@ export default function OurTeam() {
                             <div class="img-wrapper">
                             <Avatar
                                     alt="Remy Sharp"
-                                    src="R pic.jpg"
+                                    src="d pic.jpg"
                                     sx={{ width: 270, height: 270 }}
                                 />
                             </div>
                             <p class="text-blk name mt-3">
-                                Deepak Tomar
+                                Deepak Singh Tomar
                             </p>
                             <p class="text-blk position">
                                 Web Developer
@@ -77,64 +77,9 @@ export default function OurTeam() {
                             </div>
                         </div>
                     </div>
-                    <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 team-card-container">
-                        <div class="team-card">
-                            <div class="img-wrapper">
-                            <Avatar
-                             
-                                    alt="Remy Sharp"
-                                    src="R pic.jpg"
-                                    sx={{ width: 270, height: 270 }}
-                                />
-                            </div>
-                            <p class="text-blk name mt-3">
-                                Apurv Sharma
-                            </p>
-                            <p class="text-blk position ">
-                                Web Developer
-                            </p>
-                            <div class="social-media-links mb-4">
-                                <a href="http://www.twitter.com/" target="_blank">
-                                    <img src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-twitter.svg" />
-                                </a>
-                                <a href="http://www.facebook.com/" target="_blank">
-                                    <img src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-fb.svg" />
-                                </a>
-                                <a href="http://www.instagram.com/" target="_blank">
-                                    <img src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-insta.svg" />
-                                </a>
-                                <a href="http://www.gmail.com/" target="_blank">
-                                    <img src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-mail.svg" />
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 team-card-container" />
-                    {/* <div class="team-card">
-                        <div class="img-wrapper">
-                            <img class="team-img" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/expert2.png" />
-                        </div>
-                        <p class="text-blk name">
-                            Maxwell Doe
-                        </p>
-                        <p class="text-blk position">
-                            Instructor
-                        </p>
-                        <div class="social-media-links">
-                            <a href="http://www.twitter.com/" target="_blank">
-                                <img src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-twitter.svg" />
-                            </a>
-                            <a href="http://www.facebook.com/" target="_blank">
-                                <img src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-fb.svg" />
-                            </a>
-                            <a href="http://www.instagram.com/" target="_blank">
-                                <img src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-insta.svg" />
-                            </a>
-                            <a href="http://www.gmail.com/" target="_blank">
-                                <img src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-mail.svg" />
-                            </a>
-                        </div>
-                    </div> */}
+                    
+                
+       
                 </div>
             </div>
         </div>
